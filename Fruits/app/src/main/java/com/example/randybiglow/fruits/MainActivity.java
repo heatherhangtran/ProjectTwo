5eaec48d0a3d54b0ddcc3491e508b0403a39f6d7
Randy Biglow <RandyBiglow@Randys-MacBook-Pro.local>
2016-05-06 02:25:29 -0700

    Seeds database.

--- Fruits/app/src/main/java/com/example/randybiglow/fruits/MainActivity.java
+++ Fruits/app/src/main/java/com/example/randybiglow/fruits/MainActivity.java
@@ -1,7 +1,9 @@
 package com.example.randybiglow.fruits;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,22 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        DatabaseHandler db = new DatabaseHandler(this);
+
+        //Seeding database
+        db.addFruits(new Fruits("Jackfruit", "Asia", "Summer","antibacterial","yellow pieces of heaven"));
+        db.addFruits(new Fruits("Mangosteen", "Asia", "Fall", "diuretic", "purple exterior with white flesh"));
+        db.addFruits(new Fruits("Cherimoya", "North America", "Spring", "anti-inflammatory","sweet custard"));
+        db.addFruits(new Fruits("Lychee", "Asia", "Winter", "anti-acid", "crispy grapes"));
+        db.addFruits(new Fruits("Pitaya", "Asia", "Fall", "anti-aging", "mild kiwi"));
+        db.addFruits(new Fruits("Waterapple", "Asia", "Spring", "anti-diabetes", "tangy apple"));
+        db.addFruits(new Fruits("Breadfruit", "Africa", "Summer", "antioxidants", "spongy"));
+        db.addFruits(new Fruits("Soursop", "South America", "Spring","antimicrobial", "sweet and tangy custard"));
+        db.addFruits(new Fruits("Redcurrant", "Europe", "Summer", "anti-coagulant", "bright red goodness"));
+        db.addFruits(new Fruits("Finger Lime", "Australia", "Winter", "antioxidant", "beautiful sour beings"));
+        db.addFruits(new Fruits("Ganga", "North America", "Spring", "anything", "buds, lots of buds"));
+
+        List<Fruits> fruits = db.getAllFruits();
     }
 }
